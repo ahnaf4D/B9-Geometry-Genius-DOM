6eaf9025febf32d054b5ee4c70e35ecbfe38e6c5
ahnaf4D <ahnaftahmid802@gmail.com>
2024-02-12 22:40:18 +0600

    All files updated

---
+++ js/triangle-calculate.js
@@ -0,0 +1,9 @@
+/**
+ * Objective : get base and height of a triangle. Calculate the area by using the provided formula. and then display the area.
+ * step 01 : get base value
+ * 
+ */
+
+function calculateTriangleArea() {
+    console.log('inside function');
+}
