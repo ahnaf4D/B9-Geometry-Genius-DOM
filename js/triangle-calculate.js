af203cac867a117bbd20c2280538d5273727a927
ahnaf4D <ahnaftahmid802@gmail.com>
2024-02-12 23:06:08 +0600

    Triangle completed

--- js/triangle-calculate.js
+++ js/triangle-calculate.js
@@ -8,12 +8,19 @@
  */
 
 function calculateTriangleArea() {
+    // get the width value
     const triangleBaseInput = document.getElementById('triangle-base');
     const triangleBaseText = triangleBaseInput.value;
     const base = parseFloat(triangleBaseText);
-    console.log(base);
+    // console.log(base);
+    // Get the height value
     const triangleHeightInput = document.getElementById('triangle-height');
     const triangleHeightText = triangleHeightInput.value;
     const height = parseFloat(triangleHeightText);
-    console.log(height);
+    // console.log(height);
+    // Total area
+    const area = 0.5 * base * height;
+    // display triangle area
+    const triangleAreaSpan = document.getElementById('triangle_area');
+    triangleAreaSpan.innerText = area;
 }
