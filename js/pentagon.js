508a8c3f64579e2b3905214a8a031a050bb2a1e6
ahnaf4D <ahnaftahmid802@gmail.com>
2024-02-13 09:28:48 +0600

    All code updated

--- js/pentagon.js
+++ js/pentagon.js
@@ -1,11 +1,20 @@
 function pentagonAreaCalculation() {
-    const pMainValue = document.getElementById('pentagon-value1');
-    const pValue = pMainValue.value;
-    const p = pValue;
-    const bMainValue = document.getElementById('pentagon-value2');
-    const bValue = bMainValue.value;
-    const b = bValue;
-    const area = 0.5 * p * b;
-    const pentagonArea = document.getElementById('pentagon-area');
-    pentagonArea.innerText = area;
+    const perimeter = getInputValueById('pentagon-value1');
+    console.log(perimeter);
+    const apothem = getInputValueById('pentagon-value2');
+    console.log(apothem);
+    const area = 0.5 * perimeter * apothem;
+    const pentagonArea = setInnerTextByID('pentagon-area', area);
+    return pentagonArea;
+}
+function getInputValueById(inputFieldId) {
+    const inputField = document.getElementById(inputFieldId);
+    const inputValueText = inputField.value;
+    const value = parseFloat(inputValueText);
+    return value;
+}
+function setInnerTextByID(elementId, value) {
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+    return element;
 }
