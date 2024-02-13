686403732f9e1c0764490f7ba40227dea9a51998
ahnaf4D <ahnaftahmid802@gmail.com>
2024-02-13 09:42:54 +0600

    All code updated

--- js/ellipse.js
+++ js/ellipse.js
@@ -1,12 +1,18 @@
 function ellipseAreaCalculation() {
     const PI = 3.14;
-    const ellipseMainValue1 = document.getElementById('ellipse-value1');
-    const ellipseValue1 = ellipseMainValue1.value;
-    const a = parseFloat(ellipseValue1);
-    const ellipseMainValue2 = document.getElementById('ellipse-value2');
-    const ellipseValue2 = ellipseMainValue2.value;
-    const b = parseFloat(ellipseValue2);
-    const area = PI * a * b;
-    const ellipseArea = document.getElementById('ellipse-area');
-    ellipseArea.innerText = area;
+    const majorRadius = getValueFromInput('ellipse-value1');
+    const minorRadius = getValueFromInput('ellipse-value2');
+    const calculation = PI * majorRadius * minorRadius;
+    const showInDisplay = setValueToUI('ellipse-area', calculation);
+}
+function getValueFromInput(elementId) {
+    const elementMainContent = document.getElementById(elementId);
+    const elementTextValue = elementMainContent.value;
+    const elementValue = parseFloat(elementTextValue);
+    return elementValue;
+}
+function setValueToUI(element, value) {
+    const elementSelect = document.getElementById(element);
+    elementSelect.innerText = value;
+    return elementSelect;
 }
